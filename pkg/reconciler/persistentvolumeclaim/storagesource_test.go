51a3a069517a4f5743f8be584436e9fc5156a53f
Simona Pencea <simona@xata.io>
2026-02-20 17:18:31 +0100

    fix: imports

--- pkg/reconciler/persistentvolumeclaim/storagesource_test.go
+++ pkg/reconciler/persistentvolumeclaim/storagesource_test.go
@@ -24,7 +24,6 @@ import (
 	"time"
 
 	volumesnapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumesnapshot/v1"
-	"github.com/xataio/xata-cnpg/internal/scheme"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
@@ -32,6 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	apiv1 "github.com/xataio/xata-cnpg/api/v1"
+	"github.com/xataio/xata-cnpg/internal/scheme"
 	"github.com/xataio/xata-cnpg/pkg/utils"
 
 	. "github.com/onsi/ginkgo/v2"

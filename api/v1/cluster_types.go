7fa07cd8268710d80a18300d880aa9ea53f9b34f
Simona Pencea <simona@xata.io>
2026-04-02 17:19:04 +0200

    feat: add fields to cluster spec

--- api/v1/cluster_types.go
+++ api/v1/cluster_types.go
@@ -2183,7 +2183,7 @@ const (
 )
 
 // BackupConfiguration defines how the backup of the cluster are taken.
-// The supported backup methods are BarmanObjectStore and VolumeSnapshot.
+// The supported backup methods are BarmanObjectStore, VolumeSnapshot and PgBackRest.
 // For details and examples refer to the Backup and Recovery section of the
 // documentation
 type BackupConfiguration struct {
@@ -2194,6 +2194,10 @@ type BackupConfiguration struct {
 	// The configuration for the barman-cloud tool suite
 	// +optional
 	BarmanObjectStore *BarmanObjectStoreConfiguration `json:"barmanObjectStore,omitempty"`
+
+	// The configuration for pgbackrest
+	// +optional
+	PgBackRest *PgBackRestConfiguration `json:"pgBackRest,omitempty"`
 
 	// RetentionPolicy is the retention policy to be used for backups
 	// and WALs (i.e. '60d'). The retention policy is expressed in the form
--- go.mod
+++ go.mod
@@ -42,6 +42,7 @@ require (
 	go.yaml.in/yaml/v3 v3.0.4
 	golang.org/x/term v0.37.0
 	google.golang.org/grpc v1.77.0
+	gopkg.in/ini.v1 v1.67.1
 	k8s.io/api v0.34.2
 	k8s.io/apiextensions-apiserver v0.34.2
 	k8s.io/apimachinery v0.34.2
